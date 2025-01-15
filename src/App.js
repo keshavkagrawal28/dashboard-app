c27e69c4e3ff59c112caea2d14657c839650e098
keshavkagrawal28 <keshavkagrawal28@gmail.com>
2025-01-15 22:36:06 +0530

    added performance in path

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Daily from 'scenes/Daily';
 import Monthly from 'scenes/Monthly';
 import Breakdown from 'scenes/Breakdown';
 import Admin from 'scenes/Admin';
+import Performance from 'scenes/Performance';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -35,6 +36,7 @@ function App() {
               <Route path='/monthly' element={<Monthly />} />
               <Route path='/breakdown' element={<Breakdown />} />
               <Route path='/admin' element={<Admin />} />
+              <Route path='/performance' element={<Performance />} />
             </Route>
           </Routes>
         </ThemeProvider>
